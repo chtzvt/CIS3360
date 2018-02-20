3f9cb2eab45005e0cd8181c0fec7ca91766923e1
aleccoder <alexcote1rocks@gmail.com>
2018-02-20 12:36:31 -0500

    added varrible file handling logic + comments
    you prob need to fix travis to input the files

--- Project_1/src/project_1_main.c
+++ Project_1/src/project_1_main.c
@@ -8,34 +8,43 @@
 #include <stdlib.h>
 
 #define MAXSIZE 1000
-int main() {
+//defines max character array size
+int main(int argc, char** argv) {
+    // takes arguments from command line
     FILE *fp;
-    char plaintext[MAXSIZE];
-    char* plainfile = "../plainText1.txt";
 
+    char plaintext[MAXSIZE];
+    //char* plainfile = "../plainText1.txt"; //staticly defines file name for plain text
+    char* plainfile = argv[1];
+    // take command line arguments for name of plain text file
     fp = fopen(plainfile, "r");
     if (fp == NULL){
         printf("Could not open file %s",plainfile);
         return 1;
     }
+    // opens and catches for bad files
     puts("the plaintext input is");
     while (fgets(plaintext, MAXSIZE, fp) != NULL)
         printf("%s", plaintext);
+    // prints out file to io stream
     fclose(fp);
 
 
         FILE *key;
         char keytext[MAXSIZE];
-        char* keyfile = "../viginere key1.txt";
-
+      //  char* keyfile = "../viginere key1.txt"; //staticly defines file name for key text
+    char* keyfile = argv[2];
+    // takes name of keyfile argument and stores as string
         key = fopen(keyfile, "r");
         if (key == NULL){
             printf("Could not open file %s",keyfile);
             return 1;
         }
+    // opens and catches for bad files
         puts("the key input is");
         while (fgets(keytext, MAXSIZE, key) != NULL)
             printf("%s", keytext);
+    // prints out what was inputed by user into io stream
         fclose(key);
     
     return 0;
