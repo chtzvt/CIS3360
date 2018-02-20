b2f3f668d923196cd96d1787bed6c17e4492d5fe
aleccoder <alexcote1rocks@gmail.com>
2018-02-20 12:23:22 -0500

    added file handling logic

--- Project_1/src/project_1_main.c
+++ Project_1/src/project_1_main.c
@@ -5,8 +5,38 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-  puts("hello, world!");
-  return 0;
+#define MAXSIZE 1000
+int main() {
+    FILE *fp;
+    char plaintext[MAXSIZE];
+    char* plainfile = "../plainText1.txt";
+
+    fp = fopen(plainfile, "r");
+    if (fp == NULL){
+        printf("Could not open file %s",plainfile);
+        return 1;
+    }
+    puts("the plaintext input is");
+    while (fgets(plaintext, MAXSIZE, fp) != NULL)
+        printf("%s", plaintext);
+    fclose(fp);
+
+
+        FILE *key;
+        char keytext[MAXSIZE];
+        char* keyfile = "../viginere key1.txt";
+
+        key = fopen(keyfile, "r");
+        if (key == NULL){
+            printf("Could not open file %s",keyfile);
+            return 1;
+        }
+        puts("the key input is");
+        while (fgets(keytext, MAXSIZE, key) != NULL)
+            printf("%s", keytext);
+        fclose(key);
+    
+    return 0;
 }
