1a22d7eae5d2b3eee0f123aca99e00462d40395f
Charlton Trezevant <ct@ctis.me>
2018-03-06 04:29:46 -0500

    add js cipher test code (completed!)

---
+++ Project_1/js_ciph_tests.js
@@ -0,0 +1,28 @@
+function encipher_char(msgchar, keychar){
+  console.log("Code for msg " + msgchar + ": " + msgchar.charCodeAt());
+  console.log("Code for key " + keychar + ": " + keychar.charCodeAt() + " (shift: " + ((keychar.charCodeAt() - 96)%26) + ")");
+  console.log("Code for " + msgchar + " using " + keychar + ": " + (95 + ((msgchar.charCodeAt() - 96) + ((keychar.charCodeAt() - 96)%26))));
+  return String.fromCharCode(((msgchar.charCodeAt() + keychar.charCodeAt() - 194) % 26) + 97);
+}
+
+function encipher(plaintext, key, msglen) {
+  var ciphertext = [];
+  ciphertext[msglen-1] = '\0';
+
+  for(var i = 0; i < msglen; i++)
+    ciphertext[i] = encipher_char(plaintext[i], key[i]);
+
+  return ciphertext;
+}
+
+var str = "";
+var plaintext = 'wikipediaisawebbasedfreecontentencyclopediaprojectsupportedbythewikimediafoundationandbasedonamodelofopenlyeditablecontentandisrankedthefifthmostpopularwebsitexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+var keytext = 'thisbigbadisaconnivingmalepoliticianhehastakenovertheuniversewithanewinventionhecommandsasmallteamofeliteunderlingsandcanonlybedefeatedthroughwitandstrategyhecommandsasmallteamofeliteunderlingsandcanonlybedefeatedthroughwitandstrategy';
+enc = encipher(plaintext,keytext,plaintext.length);
+
+for(item of enc)
+   str += "" + item;
+
+// Output: ppsaqmjjalaswgponazlsxqenschpvmmpkypsswevbazvbxzgklbtjbzoiutcppxdixmimqdeshcbqhxkczmnqesswpoylfsdqztjzxxrflhhzeiorwcbqveahnybjwuesoewxkxmztnntkampbsmerrpihqpxgljjxkapxpjxiiqbxjlcbifqbrkaboifkdpxkazxklkivybabcbxqbaqeolrdetfqxkapqoxqbdv
+
+console.log(str);
