4e311d5ff761304e728c72bdf41c313374ee2408
ALEXANDER MA COTE <alexcote1rocks@gmail.com>
2018-04-09 19:58:10 -0400

    guarantee length of 8.

--- Project_2/main.cpp
+++ Project_2/main.cpp
@@ -5,25 +5,63 @@
 #include <stdbool.h>
 #define MAXSIZE 10000
 int binary(int i);
+int binadd(char *v1, char *v2);
 int main(int argc, char** argv){
 char string[MAXSIZE];
 int  intchunk;
-char chunk18[8];
-char chunk28[8];
-char sum[32];
+char chunk18[9];
+char chunk28[9];
+char sum[33];
 
-char chunk116[16];
-char chunk216[16];
+char chunk116[17];
+char chunk216[17];
 
-char chunk132[32];
-char chunk232[32];
+char chunk132[33];
+char chunk232[33];
 
 	switch(argv[1][0]){
 
 		case '8':
 		strcpy(string ,givemestring(argv[2]));
-		intchunk = string[0];
-		printf("%i", binary(intchunk));
+	lprintf(string);
+		//printf("%i \n",string[0]);
+			for(int j=0 ; string[j]!=0;j++) {
+				intchunk = string[j];
+
+				sprintf(chunk18,"%i", binary(intchunk));
+				//printf("%s ",chunk18);
+
+				if( j==0){
+					intchunk = string[1];
+
+
+					sprintf(chunk28,"%i\0", binary(intchunk));
+					j++;
+				}
+				int p=strlen(chunk18);
+				//printf("%i",p);
+				if(p<8){
+				chunk18[8]=0;
+				p--;
+				int o=7;
+				for(o=7;p!=0;p--) {
+				chunk18[o]=chunk18[p];
+				o--;
+
+				}
+					o--;
+				while(o!=-1) {
+
+					chunk18[o] = '0';
+					o--;
+					//puts("i ran");
+				}
+
+
+				}
+				printf("%s \n",chunk18);
+				binadd(chunk18,chunk28);
+			}
 			//bool x;
 			//x= true + true;
 			//printf("%i",x);
@@ -78,5 +116,19 @@ else
 {
 	return (i % 2 + 10 * binary(i / 2));
 }
+
+}
+
+int binadd(char *v1, char *v2){
+
+	int vl = strlen(v1);
+
+
+	for(int x =0; x<vl;x++){
+		//printf("%s\n+\n %s \n ",v1,v2);
+
+
+
+	}
 
 }
