3fe5d4ee79c1f746c47b588f61564657f6904670
Charlton Trezevant <ct@ctis.me>
2018-04-16 18:49:16 -0400

    Work in progress...

--- Project_2/ChecksumTester.java
+++ Project_2/ChecksumTester.java
@@ -25,9 +25,37 @@ public class ChecksumTester {
     
     public static int fromBytes(byte[] input, int mask){
       int sum = 0;
+      int chunk_sz;
       
-      for(byte b : input){
-        sum += (b & 0xff);
+      switch(mask){
+        case MASK_16_BIT:
+          chunk_sz = 2;
+          break;
+          
+        case MASK_32_BIT:
+          chunk_sz = 4;
+          break;
+          
+        default: // assumes MASK_8_BIT
+          chunk_sz = 1;
+      }
+      
+      for(int chunk = 0; chunk < input.length; chunk += chunk_sz){
+        
+        byte[] byte_chunk = new byte[chunk_sz];
+        int chunk_sum = 0;
+        
+        for(int i = 0; i < chunk_sz; i++){
+          byte_chunk[i] = input[chunk + i];
+        }
+        
+        for(byte b : byte_chunk){
+          //System.out.printf("%nChunk: %d %nChunk Size: %d%n Current: [%d]%n",chunk,chunk_sz,b);
+          chunk_sum += b & 0xff;
+        }
+        
+        sum += (chunk_sum);
+        
       }
       
       return sum & mask;
@@ -41,9 +69,10 @@ public class ChecksumTester {
     ChecksumCalculator checksum = new ChecksumCalculator();
     
     int sum_8 = checksum.fromString(testString, checksum.MASK_8_BIT);
-    int sum_16 = checksum.fromString(testString, checksum.MASK_16_BIT);
+    System.out.printf("Sum 8: %s%n", Integer.toHexString(sum_8));
+    int sum_16 = checksum.fromString(testString_16, checksum.MASK_16_BIT);
 
-    System.out.printf("Sum 8: %s%nSum 16: %s%n", Integer.toHexString(sum_8), Integer.toHexString(sum_16));
+    System.out.printf("Sum 16: %s%n", Integer.toHexString(sum_16));
 
   }
   
