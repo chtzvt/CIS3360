36cfe88c4b088085806768415bfc4aebc8986293
Charlton Trezevant <ct@ctis.me>
2018-04-16 23:53:55 -0400

    She works!

--- Project_2/ChecksumTester.java
+++ Project_2/ChecksumTester.java
@@ -10,6 +10,7 @@ import java.nio.charset.StandardCharsets;
 
 public class ChecksumTester {
   
+  // I'm the operator with my Checksum calculator
   private static class ChecksumCalculator {
     
     public static class Bitmath {
@@ -24,7 +25,7 @@ public class ChecksumTester {
         
       }
       
-      public static int[] chunkBytes(byte[] input, int sum_size){
+      private static int[] chunkBytes(byte[] input, int sum_size){
         int subgroup_width = sum_size / 8;
         int[] values = new int[input.length / subgroup_width];
         int j = 0;
@@ -32,14 +33,14 @@ public class ChecksumTester {
         for(int i = 0; i < input.length; i += subgroup_width){
           switch(sum_size){
             case 16:
-              values[j] = (int) ( (short) ((input[i] << 8) | (input[i + 1] & 0xFF)) ); //this is the array of bytes to actually sum up
+              values[j] = (int)((short)((input[i] << 8) | (input[i + 1] & 0xFF)));
               break;
               
             case 32:
               values[j] = ((input[i] << 24) | (input[i + 1] << 16) | (input[i + 2] << 8) | input[i + 3]) & 0xFFFFFFFF;
               break;
               
-            default: // default: assumes MASK_8_BIT
+            default:
               values[j] = (int)(input[i] & 0xFF);
           }
           
@@ -64,16 +65,20 @@ public class ChecksumTester {
   
   public static void main(String[] args){
     
-    String testString = "AAAAAAAAAA" + System.lineSeparator();
+    String testString_8 = "AAAAAAAAAA" + System.lineSeparator();
     String testString_16 = "AAAAAAAAAA" + System.lineSeparator() + 'X';
+    String testString_32 = "AAAAAAAAAAAAAAAAA" + System.lineSeparator() + "XX";
     
     ChecksumCalculator checksum = new ChecksumCalculator();
     
-    int sum_8 = checksum.calcFromString(testString, 8);
-    System.out.printf("Sum 8: %s%n", Integer.toHexString(sum_8));
+    int sum_8 = checksum.calcFromString(testString_8, 8);
+    System.out.printf("Sum 8 of %s: %s%n", testString_8, Integer.toHexString(sum_8));
     
     int sum_16 = checksum.calcFromString(testString_16, 16);
-    System.out.printf("Sum 16: %s%n", Integer.toHexString(sum_16));
+    System.out.printf("Sum 16 of %s: %s%n", testString_16, Integer.toHexString(sum_16));
+    
+    int sum_32 = checksum.calcFromString(testString_32, 32);
+    System.out.printf("Sum 132 of %s: %s%n", testString_32, Integer.toHexString(sum_32));
 
   }
   
